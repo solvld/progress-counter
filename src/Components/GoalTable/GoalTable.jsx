afdf02aa5cdda0dab05350a57a34a1d002ad5549
solvld <sol.vld@yandex.ru>
2023-12-02 21:05:35 +0400

    edit styles

--- src/Components/GoalTable/GoalTable.jsx
+++ src/Components/GoalTable/GoalTable.jsx
@@ -7,12 +7,13 @@ import "./_goalTable.scss"
 function GoalTable() {
 
   return (
-    <div className='GoalTable'> 
-      <Goal>{colors.$red}</Goal>
-      <Goal>{colors.$purple}</Goal>
-      <Goal>{colors.$green}</Goal>
-    </div>
-  )
+		<div className='GoalTable'>
+			<Goal>{colors.$red}</Goal>
+			<Goal>{colors.$green}</Goal>
+			<Goal>{colors.$slateBlue}</Goal>
+			<Goal>{colors.$goldenYellow}</Goal>
+		</div>
+	)
 }
 
 export default GoalTable;
