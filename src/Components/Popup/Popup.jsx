73dc9353a323600e2d90cde88e3e63a9fdc6c657
solvld <sol.vld@yandex.ru>
2023-12-03 22:02:12 +0400

    create popup component

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -6,22 +6,46 @@ import "./_popup.scss"
 const colorNames = Object.keys(colors);
 
 function Popup() {
-  const [color, setColor] = useState(colors.$red);
+  const [color, setColor] = useState(colors.$grey);
   
-  const divStyle = { backgroundColor: color };
+  const goalStyle = { backgroundColor: color };
+
+  
 
   return (
-		<div className='Popup'>
-			<div className='test'
-        style={divStyle}></div>
-			{colorNames.map((colorName, index) => (
-				<button
-          className='picker'
-					onClick={() => setColor(colors[colorName])}
-					key={colorName}
-          style={{backgroundColor: colors[colorName] }}
-				>{colors.colorName}</button>
-			))}
+		<div className={'Popup show'}>
+			<div className='PopupBox'>
+				<input type='text' placeholder='Goal' />
+
+				<ul>
+					<li>
+						<label htmlFor='amount'>Amount</label>
+						<input type='number' name='amount' min='1' />
+					</li>
+					<li>
+						<label htmlFor='step'>Step</label>
+						<input type='number' name='step' min='1' />
+					</li>
+				</ul>
+
+				<div className='PickerTable'>
+					{colorNames.map((colorName, index) => (
+						<button
+							className='Picker'
+							onClick={() => setColor(colors[colorName])}
+							key={colorName}
+							style={{ backgroundColor: colors[colorName] }}
+						>
+							{colors.colorName}
+						</button>
+					))}
+				</div>
+
+				<div className='bottom'>
+					<button>Cancel</button>
+					<button style={goalStyle}>Add</button>
+				</div>
+			</div>
 		</div>
 	)
 }
