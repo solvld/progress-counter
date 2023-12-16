fb6b9e380fae714059e0ec689240b947212be7d1
solvld <sol.vld@yandex.ru>
2023-12-16 22:04:37 +0400

    bug fix

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -4,21 +4,30 @@ import { colors } from '../../colors'
 import "./_popup.scss"
 
 const colorNames = Object.keys(colors);
+const capitalizeFirstChar = str => {
+  if (str != ''){
+    str = str.split('')
+		str[0] = str[0].toUpperCase()
+		return str.join('')
+  }
+}
 
 function Popup({active, setActive, goals, setGoals}) {
   const [color, setColor] = useState(colors.$grey);
   const [goal, setGoal] = useState('');
   const [amount, setAmount] = useState('');
-  const [step, setStep] = useState('');
+  const [step, setStep] = useState(1);
+  const [cleanNumber, setCleanNumber] = useState(false);
 
-  
+  const numberCheck = number => Number(number)>0? setCleanNumber(true) : setCleanNumber(false)
   const goalStyle = { backgroundColor: color };
   const clickHandler = () => {
     setActive(false);
     setColor(colors.$grey);
     setGoal('');
     setAmount('');
-    setStep('');
+    setStep(1);
+    setCleanNumber(false)
   }
   const submitForm = (e) => {
     e.preventDefault();
@@ -39,23 +48,57 @@ function Popup({active, setActive, goals, setGoals}) {
   return (
 		<div className={active ? 'Popup active' : 'Popup'}>
 			<form onSubmit={submitForm} className='PopupBox'>
-				<input onChange={(e) => {setGoal(e.target.value)}} value={goal} type='text' name='goal' placeholder='Goal' autoComplete="off" />
+				<input
+					onChange={e => {
+						setGoal(e.target.value)
+					}}
+					value={goal && capitalizeFirstChar(goal)}
+					type='text'
+					class='capitalized'
+					name='goal'
+					placeholder='Goal'
+					autoComplete='off'
+				/>
 
 				<ul>
 					<li>
-						<label htmlFor='amount'>Amount</label>
-						<input onChange={(e) => {setAmount(e.target.value)}} value={amount} type='number' name='amount' min='1' placeholder='0' />
+						<label htmlFor='amount' style={!cleanNumber? {color : colors.$red}: {color: '#B9B9B9'}}>
+							Amount
+						</label>
+						<input
+							onChange={e => {
+								setAmount(e.target.value)
+								numberCheck(e.target.value)
+							}}
+							value={amount}
+							type='number'
+							name='amount'
+							min='1'
+							placeholder='0'
+						/>
 					</li>
 					<li>
-						<label htmlFor='step'>Step</label>
-						<input onChange={(e) => {setStep(e.target.value)}} value={step} type='number' name='step' min='1' placeholder='0' />
+						<label htmlFor='step'>
+							Step
+						</label>
+						<input
+							onChange={e => {
+								setStep(e.target.value)
+								numberCheck(e.target.value)
+							}}
+							value={step}
+							type='number'
+							name='step'
+							min='1'
+							placeholder='0'
+						/>
 					</li>
 				</ul>
 
 				<div className='PickerTable'>
 					{colorNames.map((colorName, index) => (
 						<button
-              type='button'
+							type='button'
 							className='Picker'
 							onClick={() => setColor(colors[colorName])}
 							key={colorName}
@@ -67,9 +110,17 @@ function Popup({active, setActive, goals, setGoals}) {
 				</div>
 
 				<div className='bottom'>
-					<button type="button" onClick={clickHandler}>Cancel</button>
-					<button type="submit"
-          onClick={submitForm} style={goalStyle}>Add</button>
+					<button type='button' onClick={clickHandler}>
+						Cancel
+					</button>
+					<button
+						type='submit'
+						onClick={submitForm}
+						disabled={!cleanNumber}
+						style={goalStyle}
+					>
+						Add
+					</button>
 				</div>
 			</form>
 		</div>
